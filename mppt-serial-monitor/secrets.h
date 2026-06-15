a4145c382c5412de1eaf4c19f09a7baf91b0112c
Simonas <simon@devbot.lt>
2026-06-15 12:28:47 +0300

    Fixing typo in folder.

---
+++ mppt-serial-monitor/secrets.h
@@ -0,0 +1,4 @@
+const char* WIFI_SSID = "InternetOfCrap";
+const char* WIFI_PASSWORD = "InternetOfBybis";
+const char* INVERTER_URL = "http://172.16.7.1:8090/api/update-solar-charger";
+const char* API_TOKEN = "change-me";
